c869887c20fdf3b9e0f5e2630accc95547e20dbe
wxhxbt <wxhxbt@gmail.com>
2020-03-01 00:38:46 -0500

    Update Union Find. Add Pattern

---
+++ Leetcode/Pattern Find with HashSet - Prison Cells after N Days/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        int[] Cells = {1,0,0,1,0,0,1,0};
+        int N = 1000000000;
+        System.out.println(Arrays.toString(prisonAfterNDays(Cells, N)));
+    }
+
+    //Intuition method will get RLE(Runtime Limit Exceeded) when N is big.
+    //Try to find the pattern: next day is generated totally based on the current day.
+    //So, if we have seen the generated state before, there is a loop. Just run (N % cycle) times to get the result.
+    public static int[] prisonAfterNDays(int[] cells, int N) {
+        int cycle = 0;
+        boolean hasCycle = false;
+        Set<String> set = new HashSet<>();
+        int[] prev = cells.clone();
+        for(int day=0; day<N; day++){
+            int[] cur = next(prev);
+            if(!set.contains(Arrays.toString(cur))){
+                cycle++;
+                set.add(Arrays.toString(cur));
+            }
+            else{
+                hasCycle = true;
+                break;
+            }
+            prev = cur;
+        }
+        if(hasCycle){
+            for(int day=0; day < N % cycle; day++){
+                prev = next(prev);
+            }
+        }
+        return prev;
+    }
+
+    public static int[] next(int[] prev){
+        int[] cur = new int[prev.length];
+        for(int i=1; i<prev.length - 1; i++){
+            cur[i] = prev[i-1] == prev[i+1]? 1:0;
+        }
+        return cur;
+    }
+}
---
+++ Leetcode/Union Find-Min Cost to Connect All Nodes/src/Test.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Test {
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] edges = {{1, 4}, {4, 5}, {2, 3}, {1, 2}, {1, 3}, {1, 6}, {5, 6}};
+        int[][] edgesToRepair = {{1, 2, 5}, {1, 3, 10}, {1, 6, 2}, {5, 6, 5}};
+        System.out.println(minCostToConnect(n, edges, edgesToRepair));
+    }
+
+    public static int minCostToConnect(int n, int[][] edges, int[][] edgesToRepair){
+        int cost = 0;
+        UF uf = new UF(n + 1);
+        //Trick : In order to hashset the edges, convert int[] to String
+        Set<String> set = new HashSet<>();
+        for(int[] edge : edgesToRepair){
+            String s = edge[0] + "-" + edge[1];
+            set.add(s);
+        }
+        for(int[] edge : edges){
+            if(!set.contains(edge[0] + "-" + edge[1])){
+                uf.union(edge[0], edge[1]);
+            }
+        }
+        Queue<int[]> pq = new PriorityQueue<>((t1, t2) -> t1[2] - t2[2]);
+        pq.addAll(Arrays.asList(edgesToRepair));
+        while(!pq.isEmpty() && uf.numOfSets != 2){
+            int[] cur = pq.poll();
+            if(uf.find(cur[0]) != uf.find(cur[1])){
+                uf.union(cur[0], cur[1]);
+                cost += cur[2];
+            }
+        }
+        return cost;
+    }
+}
+
